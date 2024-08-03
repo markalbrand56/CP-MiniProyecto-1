47767bc95f2a05f10e500e643c76e265caf0efdb
Mark Albrand <mark.albrand5@gmail.com>
2024-08-03 15:15:27 -0600

    Initial setup

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <omp.h>
+
+int main() {
+#pragma omp parallel
+    {
+        int id = omp_get_thread_num();
+        printf("Hello from thread %d\n", id);
+    }
+    return 0;
+}
